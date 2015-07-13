e5158663474869fd3aceafdc70a353aba323f7aa
Garrett LeSage <garrett@lesage.us>
2015-07-13 11:13:18 +0200

    added calendar widget

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,7 +1,10 @@
 //= require jquery
+//= require fullcalendar
 //= require_tree ./vendor
 //= require_tree ./lib
 
 /* Turn off parts of bootstrap that you do not need in
  * _bootstrap-includes.js */
 //= require _bootstrap-includes
+
+//= require moment
