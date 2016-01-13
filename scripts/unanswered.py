28d3a6f2c1fdc600708980739744785494452baf
Rich Bowen <rbowen@rcbowen.com>
2016-01-13 09:54:27 -0500

    Useful scripts.

---
+++ scripts/unanswered.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import urllib2
+import json
+
+# Magic
+import sys    # sys.setdefaultencoding is cancelled by site.py
+reload(sys)    # to re-enable sys.setdefaultencoding()
+sys.setdefaultencoding('utf-8')
+
+url = 'https://ask.openstack.org/en/api/v1/questions/?scope=unanswered&query=rdo&sort=age-desc'
+
+response = urllib2.urlopen(url)
+m = response.read()
+r = json.loads(m)
+questions = r['questions']
+count = r['count']
+
+print str(count) + " unanswered questions:"
+
+for q in questions:
+    print
+    print q['title']
+    print q['url']
+    print "Tags: " + ", ".join( q['tags'] )
+
+print r
+
