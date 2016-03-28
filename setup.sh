bcaa68da507fab1e2b49dc2fddce22349ecde0c2
Marc Dequènes <duck@redhat.com>
2016-03-28 11:47:17 +0900

    install gems locally

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 git submodule init && git submodule update
 sudo yum install -y ruby-devel rubygems-devel gcc-c++ curl-devel rubygem-bundler patch zlib-devel ImageMagick
-bundle install
+bundle install --path vendor/bundle
