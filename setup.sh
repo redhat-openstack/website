d2eedb135bc96b11802f781c250d56ae6799021b
Rich Bowen <rbowen@rcbowen.com>
2016-06-07 13:03:04 -0400

    red hat summit

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 git submodule init && git submodule update
-sudo yum install -y ruby-devel rubygems-devel gcc-c++ curl-devel rubygem-bundler patch zlib-devel ImageMagick
+sudo dnf install -y ruby-devel rubygems-devel gcc-c++ curl-devel rubygem-bundler patch zlib-devel ImageMagick
 bundle install
