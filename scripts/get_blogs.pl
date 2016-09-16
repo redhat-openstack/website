4d2a707f43b1f19fb45f02c3a37d3956af7af137
Rich Bowen <rbowen@rcbowen.com>
2016-09-16 10:38:11 -0400

    Add Twitter to the blog script

--- scripts/get_blogs.pl
+++ scripts/get_blogs.pl
@@ -10,12 +10,20 @@ use WWW::Shorten::Yourls;
     use XML::Simple;
     use LWP::Simple;
 
+$|++;
+
 my $url = URI->new("http://planet.rdoproject.org/atom.xml");
 
 my $feed = XML::Feed->parse($url);
 
+open (my $md, '>', 'blogs.md');
+open (my $tweets, '>', 'blogs.tweets.csv');
+
 foreach ( $feed->entries ) {
-    print "**" . $_->title . "** by " . $_->author . "\n\n";
+
+    print '.';
+
+    print $md "**" . $_->title . "** by " . $_->author . "\n\n";
     my $body = $_->content->body;
     
     # Or possibly the summary?
@@ -26,17 +34,25 @@ foreach ( $feed->entries ) {
 
     $body =~ s/^.*?<p[^>]*?>//i;
     $body =~ s!</p>.*$!!is;
-    print "> $body\n\n";
+    print $md "> $body\n\n";
 
     my $link = $_->link;
 
     # tm3.org URL shortener
-    my $shorten = "http://tm3.org/yourls-api.php?signature=APIKEYHERE&action=shorturl&url=" . $link;
+    my $shorten = "http://tm3.org/yourls-api.php?signature=9d8634af7a&action=shorturl&url=" . $link;
     my $parser = new XML::Simple;
     my $content = get $shorten or die "Unable to get $url\n";
     my $data = $parser->XMLin($content);
     my $short = $data->{shorturl};
 
-    print "Read more at [$short]($short)\n\n\n";
+    print $md "Read more at [$short]($short)\n\n\n";
+
+    print $tweets '"01/01/2016 00:00:00","' . $_->title 
+            . ' #OpenStack #RDOCommunity","'
+            . $short . '"' . "\n";
 }
 
+close $md;
+close $tweets;
+
+print "\nDone\n";
