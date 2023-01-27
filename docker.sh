a49dececeb117c26851e62d18ce2a9a09cce128a
Joel Capitao <jcapitao@redhat.com>
2023-01-27 09:48:21 +0000

    Revert "Move RDO events from rh-event repo to local"
    This reverts commit 4c92c2dab7340307acea87c20af61a644cc8f58e.
    
    Reason for revert: we have to split this patch
    
    Change-Id: I1006c624b5fea08277a3dda9d412d3f626b0ae03

--- docker.sh
+++ docker.sh
@@ -2,6 +2,10 @@
 # install everything in a docker container which should
 # permit us to have the same env on any system.
 
+# clone events submodule
+git submodule init
+git submodule update
+
 # set SElinux context to allow docker to read the source directory
 chcon -Rt svirt_sandbox_file_t source/
 chcon -Rt svirt_sandbox_file_t data/
