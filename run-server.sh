0e9f3e654bcf58c6c4fa4d2ac3cff641d0be687b
Joel Capitao <jcapitao@redhat.com>
2023-01-27 09:18:41 +0100

    Remove RDO events from rh-event repo
    Change-Id: Id77b86bd95e456bb364ef6b0a1ef8d1895fbc09e

--- run-server.sh
+++ run-server.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-if [ ! -e data/events/.git ]; then
-    echo "Please initialize your repository first with setup.sh" >&2
-    exit 1
-fi
-
 bundle install --verbose && bundle exec middleman $@
